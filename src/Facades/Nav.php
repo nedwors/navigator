fc96c797761cdcfb0f36357ed8c0ee03b9c2fdfc
sam <nedwors@gmail.com>
2021-11-23 21:02:01 +0000

    CS

--- src/Facades/Nav.php
+++ src/Facades/Nav.php
@@ -12,7 +12,7 @@ use Nedwors\Navigator\Item;
  * @method static Item item(string $name)                                     Create a new menu item
  * @method static self define(Closure $items, ?string $menu = null)           Define a menu of items
  * @method static LazyCollection<Item> items(?string $menu = null)            Retrieve items in the given menu
- * @method static string toJson(?string $menu = null, mixed $options = 0)  Retrieve items in the given menu as json, using Laravel Collection's toJson method
+ * @method static string toJson(?string $menu = null, mixed $options = 0)     Retrieve items in the given menu as json, using Laravel Collection's toJson method
  * @method static self filter(Closure $filter, ?string $menu = null)          Define how the items should be filtered upon retrieval
  * @method static self activeWhen(Closure $activeCheck, ?string $menu = null) Define what qualifies an item as active
  */
