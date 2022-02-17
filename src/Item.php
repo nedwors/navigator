8e8496d39c93e0a74451cc3a3e82cf475a956815
kamilpiech97 <kamil.piech@blumilk.pl>
2022-02-17 10:22:43 +0100

    fixed when function

--- src/Item.php
+++ src/Item.php
@@ -9,14 +9,14 @@ use Illuminate\Support\Fluent;
 use Illuminate\Support\LazyCollection;
 
 /**
- * @property      string               $name     The display name for the item
- * @property      string               $url      The full url for the item
- * @property      ?string              $heroicon The heroicon name for the item
- * @property      ?string              $icon     The icon name/path for the item
- * @property-read bool                 $active    Determine if the current item is active
- * @property-read bool                 $available Determine if the current item passes its conditions for display
+ * @property      string $name     The display name for the item
+ * @property      string $url      The full url for the item
+ * @property      ?string $heroicon The heroicon name for the item
+ * @property      ?string $icon     The icon name/path for the item
+ * @property-read bool $active    Determine if the current item is active
+ * @property-read bool $available Determine if the current item passes its conditions for display
  * @property-read LazyCollection<self> $subItems  Retrieve the item's sub menu items
- * @property-read bool                 $hasActiveDecendants Determine if any of the item's decendants are active
+ * @property-read bool $hasActiveDecendants Determine if any of the item's decendants are active
  */
 class Item extends Fluent
 {
@@ -74,14 +74,14 @@ class Item extends Fluent
 
     public function when(?bool $condition): self
     {
-        $this->conditions[] = (bool) $condition;
+        $this->conditions[] = (bool)$condition;
 
         return $this;
     }
 
     public function unless(?bool $condition): self
     {
-        return $this->when(!(bool) $condition);
+        return $this->when(!(bool)$condition);
     }
 
     /** @param Closure(self): bool $activeCheck */
@@ -126,26 +126,27 @@ class Item extends Fluent
 
     protected function active(): bool
     {
-        return is_null($this->activeCheck) ? URL::current() == URL::to($this->url ?? '') : (bool) value($this->activeCheck, $this);
+        return is_null($this->activeCheck) ? URL::current() == URL::to($this->url ?? '') : (bool)value($this->activeCheck, $this);
     }
 
     protected function available(): bool
     {
-        return collect($this->conditions)->every(fn (bool $condition) => $condition);
+        return collect($this->conditions)->every(fn(bool $condition) => $condition);
     }
 
     /** @return LazyCollection<int, self> */
     protected function getSubItems(): LazyCollection
     {
         return LazyCollection::make($this->decendants)
-            ->when($this->filter, fn (LazyCollection $items) => $items->filter($this->filter)->each->filterSubItemsUsing($this->filter))
-            ->when($this->activeCheck, fn (LazyCollection $items) => $items->each->activeWhen($this->activeCheck));
+            ->when($this->filter !== null, fn(LazyCollection $items) => $items->filter($this->filter)->each->filterSubItemsUsing($this->filter))
+            ->when($this->activeCheck !== null, fn(LazyCollection $items) => $items->each->activeWhen($this->activeCheck));
     }
 
     /** @param LazyCollection<int, self> $items */
     protected function hasActiveDecendants(LazyCollection $items): bool
     {
-        return $items->reduce(fn (bool $active, self $item) => $item->subItems->isEmpty() ? $active : $this->hasActiveDecendants($item->subItems),
+        return $items->reduce(
+            fn(bool $active, self $item) => $item->subItems->isEmpty() ? $active : $this->hasActiveDecendants($item->subItems),
             $items->contains->active
         );
     }
